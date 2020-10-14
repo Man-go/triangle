82c5705a5ca3634cb915589fa51ee4b0a061e3db
Man-go <insmer@gmail.com>
2020-10-14 11:59:04 +0200

    fix bugs

--- triangle.cpp
+++ triangle.cpp
@@ -13,9 +13,9 @@ class dot_3D {
         //dot_3D(float x, float y, float z) : x_dot3D(x), y_dot3D(y), z_dot3D(z);
         void setCoordinate(float x, float y, float z);
         void printCoordinate();
-        float get—oordinateX();
-        float get—oordinateY();
-        float get—oordinateZ();    
+        float getCoordinateX();
+        float getCoordinateY();
+        float getCoordinateZ();    
 };
 
 dot_3D::dot_3D() {
@@ -43,13 +43,13 @@ void dot_3D::setCoordinate(float x, float y, float z) {
 void dot_3D::printCoordinate() {
     cout << "x = " << x_dot3D << ", y = " << y_dot3D << ", z = " << z_dot3D;
 }
-float dot_3D::get—oordinateX() {
+float dot_3D::getCoordinateX() {
     return x_dot3D;
 }
-float dot_3D::get—oordinateY() {
+float dot_3D::getCoordinateY() {
     return y_dot3D;
 }
-float dot_3D::get—oordinateZ() {
+float dot_3D::getCoordinateZ() {
     return z_dot3D;
 }
 // class dot_3D ----------- End
@@ -102,9 +102,9 @@ void triangle_3D::setTriangle—oordinate(float x0, float y0, float z0, float x1, 
 float triangle_3D::perimeterTriangle() {
     float triangle_side01, triangle_side12, triangle_side20;
     
-    triangle_side01 = sqrt( pow((apex[0].get—oordinateX() - apex[1].get—oordinateX()), 2) + pow((apex[0].get—oordinateY() - apex[1].get—oordinateY()), 2) + pow((apex[0].get—oordinateZ() - apex[1].get—oordinateZ()), 2));
-    triangle_side12 = sqrt( pow((apex[1].get—oordinateX() - apex[2].get—oordinateX()), 2) + pow((apex[1].get—oordinateY() - apex[2].get—oordinateY()), 2) + pow((apex[1].get—oordinateZ() - apex[2].get—oordinateZ()), 2));
-    triangle_side20 = sqrt( pow((apex[2].get—oordinateX() - apex[0].get—oordinateX()), 2) + pow((apex[2].get—oordinateY() - apex[0].get—oordinateY()), 2) + pow((apex[2].get—oordinateZ() - apex[0].get—oordinateZ()), 2));
+    triangle_side01 = sqrt( pow((apex[0].getCoordinateX() - apex[1].getCoordinateX()), 2) + pow((apex[0].getCoordinateY() - apex[1].getCoordinateY()), 2) + pow((apex[0].getCoordinateZ() - apex[1].getCoordinateZ()), 2));
+    triangle_side12 = sqrt( pow((apex[1].getCoordinateX() - apex[2].getCoordinateX()), 2) + pow((apex[1].getCoordinateY() - apex[2].getCoordinateY()), 2) + pow((apex[1].getCoordinateZ() - apex[2].getCoordinateZ()), 2));
+    triangle_side20 = sqrt( pow((apex[2].getCoordinateX() - apex[0].getCoordinateX()), 2) + pow((apex[2].getCoordinateY() - apex[0].getCoordinateY()), 2) + pow((apex[2].getCoordinateZ() - apex[0].getCoordinateZ()), 2));
 
     return triangle_side01 + triangle_side12 + triangle_side20;
 }
@@ -112,13 +112,13 @@ float triangle_3D::perimeterTriangle() {
 float triangle_3D::areaTriangle() {
     dot_3D vector1, vector2;
     
-    vector1.setCoordinate(apex[1].get—oordinateX() - apex[0].get—oordinateX(), apex[1].get—oordinateY() - apex[0].get—oordinateY(), apex[1].get—oordinateZ() - apex[0].get—oordinateZ());
-    vector2.setCoordinate(apex[2].get—oordinateX() - apex[0].get—oordinateX(), apex[2].get—oordinateY() - apex[0].get—oordinateY(), apex[2].get—oordinateZ() - apex[0].get—oordinateZ());
+    vector1.setCoordinate(apex[1].getCoordinateX() - apex[0].getCoordinateX(), apex[1].getCoordinateY() - apex[0].getCoordinateY(), apex[1].getCoordinateZ() - apex[0].getCoordinateZ());
+    vector2.setCoordinate(apex[2].getCoordinateX() - apex[0].getCoordinateX(), apex[2].getCoordinateY() - apex[0].getCoordinateY(), apex[2].getCoordinateZ() - apex[0].getCoordinateZ());
     
 
-    float vector_productX = vector2.get—oordinateY() * vector1.get—oordinateZ() - vector2.get—oordinateZ() * vector1.get—oordinateY();
-    float vector_productY = vector2.get—oordinateZ() * vector1.get—oordinateX() - vector2.get—oordinateX() * vector1.get—oordinateZ();
-    float vector_productZ = vector2.get—oordinateX() * vector1.get—oordinateY() - vector2.get—oordinateY() * vector1.get—oordinateX();
+    float vector_productX = vector2.getCoordinateY() * vector1.getCoordinateZ() - vector2.getCoordinateZ() * vector1.getCoordinateY();
+    float vector_productY = vector2.getCoordinateZ() * vector1.getCoordinateX() - vector2.getCoordinateX() * vector1.getCoordinateZ();
+    float vector_productZ = vector2.getCoordinateX() * vector1.getCoordinateY() - vector2.getCoordinateY() * vector1.getCoordinateX();
 
     return sqrt(pow(vector_productX, 2) + pow(vector_productY, 2) + pow(vector_productZ, 2)) / 2;
 }
@@ -126,13 +126,13 @@ float triangle_3D::areaTriangle() {
 void triangle_3D::normalTriangle() {
     dot_3D vector1, vector2, normal, normal_new;
 
-    vector1.setCoordinate(apex[1].get—oordinateX() - apex[0].get—oordinateX(), apex[1].get—oordinateY() - apex[0].get—oordinateY(), apex[1].get—oordinateZ() - apex[0].get—oordinateZ());
-    vector2.setCoordinate(apex[2].get—oordinateX() - apex[0].get—oordinateX(), apex[2].get—oordinateY() - apex[0].get—oordinateY(), apex[2].get—oordinateZ() - apex[0].get—oordinateZ());
+    vector1.setCoordinate(apex[1].getCoordinateX() - apex[0].getCoordinateX(), apex[1].getCoordinateY() - apex[0].getCoordinateY(), apex[1].getCoordinateZ() - apex[0].getCoordinateZ());
+    vector2.setCoordinate(apex[2].getCoordinateX() - apex[0].getCoordinateX(), apex[2].getCoordinateY() - apex[0].getCoordinateY(), apex[2].getCoordinateZ() - apex[0].getCoordinateZ());
 
 
-    float vector_productX = vector2.get—oordinateY() * vector1.get—oordinateZ() - vector2.get—oordinateZ() * vector1.get—oordinateY();
-    float vector_productY = vector2.get—oordinateZ() * vector1.get—oordinateX() - vector2.get—oordinateX() * vector1.get—oordinateZ();
-    float vector_productZ = vector2.get—oordinateX() * vector1.get—oordinateY() - vector2.get—oordinateY() * vector1.get—oordinateX();
+    float vector_productX = vector2.getCoordinateY() * vector1.getCoordinateZ() - vector2.getCoordinateZ() * vector1.getCoordinateY();
+    float vector_productY = vector2.getCoordinateZ() * vector1.getCoordinateX() - vector2.getCoordinateX() * vector1.getCoordinateZ();
+    float vector_productZ = vector2.getCoordinateX() * vector1.getCoordinateY() - vector2.getCoordinateY() * vector1.getCoordinateX();
 
     float normalization = sqrt(pow(vector_productX,2) + pow(vector_productY,2) + pow(vector_productZ,2));
     
@@ -148,9 +148,9 @@ void triangle_3D::inner_corners() {
 
     float area = areaTriangle();
 
-    triangle_side01 = sqrt(pow((apex[0].get—oordinateX() - apex[1].get—oordinateX()), 2) + pow((apex[0].get—oordinateY() - apex[1].get—oordinateY()), 2) + pow((apex[0].get—oordinateZ() - apex[1].get—oordinateZ()), 2));
-    triangle_side12 = sqrt(pow((apex[1].get—oordinateX() - apex[2].get—oordinateX()), 2) + pow((apex[1].get—oordinateY() - apex[2].get—oordinateY()), 2) + pow((apex[1].get—oordinateZ() - apex[2].get—oordinateZ()), 2));
-    triangle_side20 = sqrt(pow((apex[2].get—oordinateX() - apex[0].get—oordinateX()), 2) + pow((apex[2].get—oordinateY() - apex[0].get—oordinateY()), 2) + pow((apex[2].get—oordinateZ() - apex[0].get—oordinateZ()), 2));
+    triangle_side01 = sqrt(pow((apex[0].getCoordinateX() - apex[1].getCoordinateX()), 2) + pow((apex[0].getCoordinateY() - apex[1].getCoordinateY()), 2) + pow((apex[0].getCoordinateZ() - apex[1].getCoordinateZ()), 2));
+    triangle_side12 = sqrt(pow((apex[1].getCoordinateX() - apex[2].getCoordinateX()), 2) + pow((apex[1].getCoordinateY() - apex[2].getCoordinateY()), 2) + pow((apex[1].getCoordinateZ() - apex[2].getCoordinateZ()), 2));
+    triangle_side20 = sqrt(pow((apex[2].getCoordinateX() - apex[0].getCoordinateX()), 2) + pow((apex[2].getCoordinateY() - apex[0].getCoordinateY()), 2) + pow((apex[2].getCoordinateZ() - apex[0].getCoordinateZ()), 2));
 
     cout << "1. inner corner of a triangle: " << (asin(2 * area / (triangle_side01 * triangle_side12))) * 180 / 3.141592653589793 << endl;
     cout << "2. inner corner of a triangle: " << (asin(2 * area / (triangle_side01 * triangle_side20))) * 180 / 3.141592653589793 << endl;
@@ -161,9 +161,9 @@ void triangle_3D::inner_corners() {
 void triangle_3D::center_of_gravity() {
     dot_3D grav;
     
-    float x_grav = (apex[0].get—oordinateX() + apex[1].get—oordinateX() + apex[2].get—oordinateX()) / 3;
-    float y_grav = (apex[0].get—oordinateY() + apex[1].get—oordinateY() + apex[2].get—oordinateY()) / 3;
-    float z_grav = (apex[0].get—oordinateZ() + apex[1].get—oordinateZ() + apex[2].get—oordinateZ()) / 3;
+    float x_grav = (apex[0].getCoordinateX() + apex[1].getCoordinateX() + apex[2].getCoordinateX()) / 3;
+    float y_grav = (apex[0].getCoordinateY() + apex[1].getCoordinateY() + apex[2].getCoordinateY()) / 3;
+    float z_grav = (apex[0].getCoordinateZ() + apex[1].getCoordinateZ() + apex[2].getCoordinateZ()) / 3;
 
     grav.setCoordinate(x_grav, y_grav, z_grav);
 
